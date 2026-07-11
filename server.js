cb291f186b6865350e0474bde6b6ffb4f97a458a
Muhammad Moeen <mooenmalik374@gmail.com>
2026-07-11 16:36:02 +0500

    structured start

--- server.js
+++ server.js
@@ -1,6 +1,1 @@
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.end("Hey nigga!")
-})
-
-module.exports= server;
+console.log("main chaliiii")
